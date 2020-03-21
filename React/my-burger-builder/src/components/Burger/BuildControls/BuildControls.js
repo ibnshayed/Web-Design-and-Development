064cddd30de96fd9c629e00b96167deab1dd1f01
ibnshayed <emranibnshayed@gmail.com>
2020-03-21 19:56:32 +0600

    ibnshayed

--- React/my-burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ React/my-burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -3,18 +3,26 @@ import classes from './BuildControls.css';
 import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
-    {lable: 'Salad', type:'salad'},
-    {lable: 'Bacon', type:'bacon'},
-    {lable: 'Cheese', type:'cheese'},
-    {lable: 'Meat', type:'meat'},
+    {label: 'Salad', type:'salad'},
+    {label: 'Bacon', type:'bacon'},
+    {label: 'Cheese', type:'cheese'},
+    {label: 'Meat', type:'meat'},
 ];
 
 const buildControls = (props) => {
     return (
         <div className={classes.BuildControls}>
+            <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
             {controls.map(ctrl => (
-                <BuildControl key={ctrl.lable} label={ctrl.lable}/>
+                <BuildControl
+                    key={ctrl.label}
+                    label={ctrl.label}
+                    added={() => props.ingredientAdded(ctrl.type)}
+                    remove={() => props.ingredientRemoved(ctrl.type)}
+                    disabled={props.disabled[ctrl.type]}/>
             ))}
+            <button className={classes.OrderButton}
+                disabled={!props.purchasable}>ORDER NOW</button>
         </div>
     );
 };
